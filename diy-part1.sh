ee8f0292e1fb2b24ebac4b823ce7af7e999778e9
sunksd <42551119+sunksd@users.noreply.github.com>
2020-08-12 19:16:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.gitconf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/jerrykuku/node-request.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
